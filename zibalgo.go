38813cb6582c0a6d931d2837ff6dce6a09f35d87
blackestwhite <pesaregoal@gmail.com>
2024-02-12 00:03:27 +0330

    Feat: add basic skeleton for the package with `NewPayment` and `VerifyPayment` methods

---
+++ zibalgo.go
@@ -0,0 +1,73 @@
+package zibalgo
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// NewClient creates a new ZibalClient with the provided configuration.
+func NewClient(merchant string) *ZibalClient {
+	// Initialize the HTTP client
+	httpClient := &http.Client{}
+
+	return &ZibalClient{
+		httpClient: httpClient,
+		merchant:   merchant,
+	}
+}
+
+func (c *ZibalClient) NewPayment(paymentRequest PaymentRequest) (paymentResponse PaymentResponse, err error) {
+	requestBody, err := json.Marshal(paymentRequest)
+	if err != nil {
+		return paymentResponse, fmt.Errorf("failed to marshal request data: %w", err)
+	}
+
+	url := fmt.Sprint(BaseURL, "/v1/request")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return paymentResponse, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return paymentResponse, fmt.Errorf("HTTP request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&paymentResponse)
+	if err != nil {
+		return paymentResponse, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return paymentResponse, nil
+}
+
+func (c *ZibalClient) VerifyPayment(vericationRequest VerificationRequest) (verificationResponse VerificationResponse, err error) {
+	requestBody, err := json.Marshal(vericationRequest)
+	if err != nil {
+		return verificationResponse, fmt.Errorf("failed to marshal request data: %w", err)
+	}
+
+	url := fmt.Sprint(BaseURL, "/v1/verify")
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	if err != nil {
+		return verificationResponse, fmt.Errorf("failed to create HTTP request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return verificationResponse, fmt.Errorf("HTTP request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&verificationResponse)
+	if err != nil {
+		return verificationResponse, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return verificationResponse, nil
+}
